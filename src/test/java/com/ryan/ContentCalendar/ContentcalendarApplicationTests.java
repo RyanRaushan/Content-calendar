a3f7b36476043a6fe9d36623e0755f07d8867966
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-06-09 16:19:41 +0530

    Content Calendar Application

---
+++ src/test/java/com/ryan/ContentCalendar/ContentcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.ContentCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentcalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
